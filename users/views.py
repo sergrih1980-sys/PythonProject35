c933a7a37c40e001e573b7e823dec5d26a48a982
sergrih1980-sys <sergrih1980@gmail.com>
2026-07-26 22:55:30 +0300

    JWT-авторизация

--- users/views.py
+++ users/views.py
@@ -4,6 +4,12 @@ from rest_framework import viewsets
 from .models import Payment
 from .serializers import PaymentSerializer
 from .filters import PaymentFilter
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from .serializers import UserRegistrationSerializer
+
 
 class PaymentViewSet(viewsets.ModelViewSet):
     # select_related нужен, чтобы не было N+1 запросов к БД
@@ -17,3 +23,17 @@ class PaymentViewSet(viewsets.ModelViewSet):
     # Настройка сортировки
     ordering_fields = ['paid_at', 'amount']
     ordering = ['-paid_at']
+
+
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            return Response(
+                {"detail": "Пользователь успешно зарегистрирован"},
+                status=status.HTTP_201_CREATED,
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
